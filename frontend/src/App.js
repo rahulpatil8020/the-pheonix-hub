ece7deffff23c6c4f06d5fa10a0b9b75ffdedf0a
rahulpatil8020 <rahuldpatil20@gmail.com>
2022-11-02 11:51:38 -0700

    added an image for the signup and login page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,8 @@ import "./App.css";
 import HomePage from "./pages/HomePage";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
+import LoginPage from "./pages/LoginPage";
+import SignupPage from "./pages/SignupPage";
 
 function App() {
   return (
