3b44d23de704ce95130486bdb171146fc5f6e9e1
rahulpatil8020 <rahuldpatil20@gmail.com>
2022-11-07 19:48:35 -0800

    Implemented login and signup system

--- frontend/src/pages/LoginPage.js
+++ frontend/src/pages/LoginPage.js
@@ -1,10 +1,40 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import loginImage from "../assets/loginimage.jpeg";
 import { UserContext } from "../models/user-context";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
 
 const LoginPage = () => {
   const [user, setUser] = useContext(UserContext);
-  console.log(user, "//////");
+  const [data, setData] = useState({
+    email: "",
+    password: "",
+  });
+  const navigate = useNavigate();
+  const [error, setError] = useState(null);
+  const handleChange = ({ currentTarget: input }) => {
+    setData({ ...data, [input.name]: input.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const url = "http://localhost:8000/api/v1/auth";
+      const { data: res } = await axios.post(url, data);
+      setUser(data);
+      localStorage.setItem("token", res.data);
+      navigate("/code");
+      console.log(res.message);
+    } catch (error) {
+      if (
+        error.response &&
+        error.response.status >= 400 &&
+        error.response.status <= 500
+      ) {
+        setError(error.response.data.message);
+      }
+    }
+  };
   return (
     <div className="w-full h-screen flex">
       <div className="grid grid-cols-1 md:grid-cols-2 m-auto h-[550px] shadow-lg shadow-gray-600 sm:max-w-[900px]">
@@ -16,23 +46,36 @@ const LoginPage = () => {
             <h2 className="text-4xl font-bold text-center mb-8">Pheonix</h2>
             <div>
               <input
+                required
+                onChange={handleChange}
+                name="email"
+                value={data.email}
                 className="border p-2 mr-2 my-1"
                 type="text"
                 placeholder="Username"
               />
               <input
+                required
+                onChange={handleChange}
+                name="password"
+                value={data.password}
                 className="border p-2 "
                 type="password"
                 placeholder="Password"
               />
             </div>
-            <button className="w-full py-2 my-4">Sign In</button>
+            {error && <div className="text-red-500">{error}</div>}
+            <button onClick={handleSubmit} className="w-full py-2 my-4">
+              Sign In
+            </button>
             <p className="text-center cursor-default hover:text-indigo-600">
               Forgot Username or Password?
             </p>
-            <p className="my-2 text-center underline text-indigo-600 cursor-pointer">
-              Sign Up
-            </p>
+            <Link to="/signup">
+              <p className="my-2 text-center underline text-indigo-600 cursor-pointer">
+                Sign Up
+              </p>
+            </Link>
           </form>
         </div>
       </div>
--- frontend/src/pages/SignupPage.js
+++ frontend/src/pages/SignupPage.js
@@ -1,7 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import loginImage from "../assets/loginimage.jpeg";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
 
 const SignupPage = () => {
+  const [data, setData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+  });
+  const [error, setError] = useState(null);
+
+  const handleChange = ({ currentTarget: input }) => {
+    setData({ ...data, [input.name]: input.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const url = "http://localhost:8000/api/v1/users";
+      const { data: res } = await axios.post(url, data);
+      console.log(data);
+      navigate("/login");
+      console.log(res.message);
+    } catch (error) {
+      if (
+        error.response &&
+        error.response.status >= 400 &&
+        error.response.status <= 500
+      ) {
+        setError(error.response.data.message);
+      }
+    }
+  };
+
+  const navigate = useNavigate();
   return (
     <div className="w-full h-screen flex">
       <div className="grid grid-cols-1 md:grid-cols-2 m-auto h-[550px] shadow-lg shadow-gray-600 sm:max-w-[900px]">
@@ -9,38 +43,59 @@ const SignupPage = () => {
           <img className="w-full h-full" src={loginImage} alt="/" />
         </div>
         <div className="p-4 flex flex-col justify-around">
-          <form action="">
+          <form action="submit">
             <h2 className="text-4xl font-bold text-center mb-8">Pheonix</h2>
             <div className="grid grid-cols-2 m-auto">
               <input
+                required
+                onChange={handleChange}
+                name="firstName"
+                value={data.firstName}
                 className="border p-2 mr-2 my-1"
                 type="text"
                 placeholder="First Name"
               />
               <input
+                required
+                onChange={handleChange}
+                name="lastName"
+                value={data.lastname}
                 className="border p-2 mr-2 my-1"
                 type="text"
                 placeholder="Last Name"
               />
               <input
+                required
+                onChange={handleChange}
+                name="email"
+                value={data.email}
                 className="border p-2 mr-2 my-1"
                 type="text"
                 placeholder="Username"
               />
               <input
-                className="border p-2 "
+                required
+                onChange={handleChange}
+                name="password"
+                value={data.password}
+                className="border p-2 mr-2 my-1"
                 type="password"
                 placeholder="Password"
               />
             </div>
-            <button className="w-full py-2 my-4">Sign Up</button>
+            {error && <div className="text-red-500">{error.message}</div>}
+            <button onClick={handleSubmit} className="w-full py-2 my-4">
+              Sign Up
+            </button>
 
             <p className="text-center cursor-default hover:text-indigo-600">
               Already Have An Account?
             </p>
-            <p className="text-center underline text-indigo-600 cursor-pointer">
-              Sign Up
-            </p>
+            <Link to="/login">
+              <p className="text-center underline text-indigo-600 cursor-pointer">
+                Login
+              </p>
+            </Link>
           </form>
         </div>
       </div>
