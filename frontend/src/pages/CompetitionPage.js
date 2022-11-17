98e0f53d07201f9cdfad29c995ef9adf42035ebf
rahulpatil8020 <rahuldpatil20@gmail.com>
2022-11-16 18:16:02 -0800

    passing query parameter through get request to get the questions of specific competition only

--- frontend/src/pages/CompetitionPage.js
+++ frontend/src/pages/CompetitionPage.js
@@ -1,15 +1,20 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-
-const CompetitionPage = () => {
+const CompetitionPage = (props) => {
   const [questions, setQuestions] = useState([]);
   const getAllQuestions = async () => {
     try {
       const url = `http://localhost:8000/api/v1${window.location.pathname}/questions`;
-      await axios.get(url, {}).then((res) => {
-        console.log(res, "###");
-        if (res) setQuestions(res.data);
-      });
+      await axios
+        .get(url, {
+          params: {
+            competitionId: window.location.pathname.substring(14),
+          },
+        })
+        .then((res) => {
+          console.log(res, "###");
+          if (res) setQuestions(res.data);
+        });
     } catch (error) {
       console.log(error);
     }
@@ -17,7 +22,6 @@ const CompetitionPage = () => {
   useEffect(() => {
     getAllQuestions();
   }, []);
-  console.log(questions, "....");
   return <div>{questions[0]?.name}</div>;
 };
 
