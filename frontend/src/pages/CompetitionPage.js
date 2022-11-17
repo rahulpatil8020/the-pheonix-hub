960607c3dc52d595fe192840e0c9c03ddc5db2ab
rahulpatil8020 <rahuldpatil20@gmail.com>
2022-11-16 17:04:58 -0800

    Got questions data in competition page

--- frontend/src/pages/CompetitionPage.js
+++ frontend/src/pages/CompetitionPage.js
@@ -1,7 +1,24 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const CompetitionPage = () => {
-  return <div>CompetitionPage</div>;
+  const [questions, setQuestions] = useState([]);
+  const getAllQuestions = async () => {
+    try {
+      const url = `http://localhost:8000/api/v1${window.location.pathname}/questions`;
+      await axios.get(url, {}).then((res) => {
+        console.log(res, "###");
+        if (res) setQuestions(res.data);
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    getAllQuestions();
+  }, []);
+  console.log(questions, "....");
+  return <div>{questions[0]?.name}</div>;
 };
 
 export default CompetitionPage;
